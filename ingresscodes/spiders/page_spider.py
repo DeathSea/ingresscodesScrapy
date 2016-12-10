122f534eedbe4c9d628d9f67e956e6e30290dabf
DeathSea <coder.deathsea@gmail.com>
2016-12-10 20:04:02 +0800

    fix error

--- ingresscodes/spiders/page_spider.py
+++ ingresscodes/spiders/page_spider.py
@@ -10,12 +10,13 @@ class pageSpider(scrapy.Spider):
 			yield scrapy.Request(post.css("a::attr(href)").extract_first(),callback = self.parse_post)
 		next_page = response.css("nav.navigation div.nav-links a.next::attr(href)").extract_first()
 
-		self.log("the next page:"+next_page)
+		# self.log("the next page:"+next_page)
 		if next_page is not None:
 			yield scrapy.Request(next_page, callback = self.parse)
 
 
 	def parse_post(self,response):
+		#fuck window dir
 		this_post_title = response.css("h1.entry-title::text").extract_first().replace("|","").replace("<","").replace(">","").replace("\\","").replace("/","").replace(":","").replace("?","").replace("*","").replace("\"","")
 		this_post_content = response.css("div.entry-content").extract_first().encode("utf-8")
 		tags = []
@@ -25,10 +26,13 @@ class pageSpider(scrapy.Spider):
 		# self.log(u"this content is:"+this_post_content)
 		if u"Daily Codes" in tags:
 			#invest ingress code
-			code_collection = response.css("code::text").re("\[\w{1,2}\]\xa0([a-zA-Z0-9]*)")
+			code_collection = response.css("code::text").re("\[\w{1,2}\][\xa0 ](.*)")
 			with open("code_post_map.txt","a") as f:
 				for code in code_collection:
-					print >> f,code,this_post_title.encode("utf-8")
+					try:
+						print >> f,code.encode("u8"),"\t",this_post_title.encode("utf-8"),"\t",response.url,"\t","code\\%s.html"%this_post_title.encode("u8")
+					except:
+						self.log("error while write passcode "+code+" in post : "+response.url)
 
 			with open("code\\%s.html"%this_post_title,"w") as f:
 				f.write(this_post_content)
@@ -36,9 +40,13 @@ class pageSpider(scrapy.Spider):
 
 		elif u"Ingress Report" in tags:
 			#ingress report code
+			code_collection = response.css("code::text").re("\[\w{1,2}\][\xa0 ](.*)")
 			with open("code_post_map.txt","a") as f:
 				for code in code_collection:
-					print >> f,code,this_post_title.encode("utf-8")
+					try:
+						print >> f,code.encode("u8"),"\t",this_post_title.encode("utf-8"),"\t",response.url,"\t","code\\%s.html"%this_post_title.encode("u8")
+					except:
+						self.log("error while write passcode "+code+" in post : "+response.url)
 
 			with open("code\\%s.html"%this_post_title,"w") as f:
 				f.write(this_post_content)
