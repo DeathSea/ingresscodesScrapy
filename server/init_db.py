9e36e0bfd7d31c9923765700b811543bbfd999b3
DeathSea <coder.deathsea@gmail.com>
2016-12-11 14:53:53 +0800

    a server

---
+++ server/init_db.py
@@ -0,0 +1,35 @@
+import sqlite3
+db = sqlite3.connect("test.db")
+
+c = db.cursor()
+
+c.execute("""create table if not exists passcode (
+	id INTEGER PRIMARY KEY autoincrement,
+	passcode BLOB,
+	title TEXT,
+	source TEXT,
+	filename TEXT,
+	decode_method_tags TEXT
+	)""")
+c.execute("delete from passcode;")
+c.execute("update sqlite_sequence set seq=1 where name=\"passcode\"")
+content = None
+with open('..\\code_post_map.txt','r') as f:
+	content = f.read()
+rows = []
+if content is not None:
+	k = False
+	for line in content.splitlines():
+		if not k:
+			k = True
+			continue#skip first line
+		if line:
+			row = []
+			for col in line.split("\t"):
+				row.append(col.strip().decode("u8"))
+			rows.append(tuple(row))
+print rows
+c.executemany("insert into passcode values (NULL,?,?,?,?,?)",rows)
+c.close()
+db.commit()
+db.close()
---
+++ server/main.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8
+import web,os
+        
+urls = (
+    '/', 'index',
+    '/code/(.*?).html','html',
+)
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+db = web.database(dbn='sqlite',db='test.db')
+# for x in db.select('passcode'):
+# 	print x
+class index:
+    def GET(self):
+    	c = db.select('passcode')
+        return render.test(content=c)
+
+class html:
+	def GET(self,html_name):
+		content = '404 not found'
+		print html_name
+		try:
+			f = open('..\\code\\%s.html'%html_name,"r")
+			content = f.read()
+			f.close()
+		except p:
+			print p
+		return render.pagemodel(title=html_name,content=content)
+
+if __name__ == "__main__":
+    app.run()
